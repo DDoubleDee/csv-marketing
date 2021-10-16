7c28312d6027ed7380aabdbe5f8f443fd2617262
DDoubleDee <91531619+DDoubleDee@users.noreply.github.com>
2021-10-16 11:34:03 +0600

    Add files via upload

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,11 +1,18 @@
-from scripts import readcsv, getengine, tosql, filtersql, getmetad, connectdb, getinfo
+from scripts import Scripts
+from dotenv import load_dotenv
 import os
 
 
-inp = readcsv('marketing_campaign.csv')  # Read file
-engine = getengine(getinfo())  # Get engine info
-tosql(engine, inp)  # Write input to db
-metadata = getmetad()  # Get metadata
-connection = connectdb(engine)  # Get connection info
-filterinp = 4  # 0 = no filter; 1 = wine => 1000; 2 = web > shop; 3 = shop > web; 4 = phd is true; 5 = singles
-filtersql(metadata, engine, connection, filterinp)
+load_dotenv()
+script = Scripts(os.getenv('CONNECTION_URL'))
+script.read_csv_file('marketing_campaign.csv')
+script.create_extra_dataframes()
+script.apply_most_expensive_and_keys()
+script.send_data_to_sql()
+i = 5
+# If == 1 creates file with customers who paid more than 1000 for wine
+# If == 2 creates file with customers who buy more from the web
+# If == 3 creates file with customers who buy more from the store
+# If == 4 creates file with customers who have PhD
+# If == 5 creates file with customers who are single
+script.query_sql(5)
--- pythonProject1/scripts.py
+++ pythonProject1/scripts.py
@@ -1,71 +1,99 @@
 import pandas
 import sqlalchemy as db
+from decimal import Decimal
 
 
-def getinfo():
-    return open('.env', 'r').read()
+# Calculates the most bought product and then applies it's name to Most_bought
+def calculate_most_bought(dataframerow):
+    temporary_variable = 0
+    # This for loop goes through the list of possible product expenses and finds the most bought one
+    # Then adds a new column with that product's name
+    for item in ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']:
+        if dataframerow['Mnt{0}'.format(item)] > temporary_variable:
+            temporary_variable = dataframerow['Mnt{0}'.format(item)]
+            dataframerow['Most_Bought'] = item
+    return dataframerow
 
 
-def readcsv(csv):
-    inp = pandas.read_csv(csv, sep=r'\t', engine='python')
-    for i in range(1, 6):
-        inp['AcceptedCmp{0}'.format(i)] = inp['AcceptedCmp{0}'.format(i)].astype('boolean')
-    inp['Complain'] = inp['Complain'].astype('boolean')
-    inp['Response'] = inp['Response'].astype('boolean')
-    inp['Education'] = inp['Education'].astype('category')
-    inp['Marital_Status'] = inp['Marital_Status'].astype('category')
-    inp['Income'] = inp['Income'].astype('float')
-    mnt = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
-    for i in mnt:
-        inp['Mnt{0}'.format(i)] = inp['Mnt{0}'.format(i)].astype('float')
-    return inp
+# Replaces all default values of Education_key and Marital_Status_key with values dependent on the Education
+# or Marital_Status columns in dataframe row
+def add_foreign_keys(dataframerow):
+    # This loop goes through the list of possible educations and then applies that education's id to use as a foreign key
+    for education in [['PhD', 0], ['Graduation', 1], ['Master', 2], ['2n Cycle', 3], ['Basic', 4]]:
+        if dataframerow['Education'] == education[0]:
+            dataframerow['Education_key'] = education[1]
+    # This loop goes through the list of possible marital statuses and then applies that marital statuses' id to use as a foreign key
+    for marital_status in [['Single', 0], ['Married', 1], ['Together', 2], ['Divorced', 3]]:
+        if dataframerow['Marital_Status'] == marital_status[0]:
+            dataframerow['Marital_Status_key'] = marital_status[1]
+    return dataframerow
 
 
-def getengine(confo):
-    return db.create_engine(confo)
+# Converts all amounts in dataframe row to Decimal()
+def convert_to_decimal(dataframerow):
+    for item in ['Income', 'MntWines', 'MntFruits', 'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts', 'MntGoldProds']:
+        dataframerow[item] = Decimal(dataframerow[item])
+    return dataframerow
 
 
-def getmetad():
-    return db.MetaData()
+class Scripts:
+    # Creates an engine, connects to the database and grabs metadata
+    def __init__(self, connectioninfo):
+        self.engine = db.create_engine(connectioninfo)
+        self.metadata = db.MetaData()
+        self.connection = self.engine.connect()
 
+    # Reads the csv file using pandas' read_csv(). then converts 1/0 in it to boolean True/False, after which
+    # uses apply() to convert all price integers to Decimal()
+    def read_csv_file(self, csvfilename):
+        self.filedata = pandas.read_csv(csvfilename, sep=r'\t', engine='python')
+        for number in range(1, 6):
+            self.filedata['AcceptedCmp{0}'.format(number)] = self.filedata['AcceptedCmp{0}'.format(number)].astype('bool')
+        self.filedata['Complain'] = self.filedata['Complain'].astype('bool')
+        self.filedata['Response'] = self.filedata['Response'].astype('bool')
+        self.filedata = self.filedata.apply(convert_to_decimal, axis=1)
 
-def connectdb(engine):
-    return engine.connect()
+    # This creates two extra dataframes to later send to sql using the pandas' to_sql() function
+    def create_extra_dataframes(self):
+        self.education_dataframe = pandas.DataFrame(data={'Name': ['PhD', 'Graduation', 'Master', '2n Cycle', 'Basic']})
+        self.marital_status_dataframe = pandas.DataFrame(data={'Name': ['Single', 'Married', 'Together', 'Divorced']})
 
+    # This uses the pandas apply() function to iterate over the dataframe and add foreign key as well as
+    # find the most bought product for each customer
+    def apply_most_expensive_and_keys(self):
+        self.filedata['Most_Bought'] = 'string'
+        self.filedata['Marital_Status_key'] = 0
+        self.filedata['Education_key'] = 0
+        self.filedata = self.filedata.apply(calculate_most_bought, axis=1)
+        self.filedata = self.filedata.apply(add_foreign_keys, axis=1)
+        self.filedata.drop(['Education'], axis=1)
+        self.filedata.drop(['Marital_Status'], axis=1)
 
-def tosql(engine, inp):
-    mnt = ['Wines', 'Fruits', 'MeatProducts', 'FishProducts', 'SweetProducts', 'GoldProds']
-    inp['MostExpense'] = 'one'
-    for i in range(2240):
-        comp = 0
-        for i2 in mnt:
-            if int(comp) < inp['Mnt{0}'.format(i2)][i]:
-                comp = inp['Mnt{0}'.format(i2)][i]
-                inp['MostExpense'][i] = i2
-    inp.to_sql('inputlist', con=engine, if_exists='replace')
-    for i in ['Phd', 'Graduation', 'Master', '2n Cycle', 'Basic']:
-        i2 = inp.loc[inp['Education'] == i]
-        i2['foreign_key'] = i2['ID']
-        i2.drop(['ID'], axis=1)
-        i2.to_sql(i, con=engine, if_exists='replace')
-    for i in ['Single', 'Married', 'Together', 'Divorced']:
-        i2 = inp.loc[inp['Marital_Status'] == i]
-        i2['foreign_key'] = i2['ID']
-        i2.drop(['ID'], axis=1)
-        i2.to_sql(i, con=engine, if_exists='replace')
+    # This uses pandas' to_sql() function to create and send three tables, or replace them if they exist
+    def send_data_to_sql(self):
+        self.filedata.to_sql('list', con=self.engine, if_exists='replace')
+        self.education_dataframe.to_sql('educations', con=self.engine, if_exists='replace')
+        self.marital_status_dataframe.to_sql('maritalstatuses', con=self.engine, if_exists='replace')
 
-
-def filtersql(metadata, engine, connection, filterinp=0):
-    table = db.Table('inputlist', metadata, autoload=True, autoload_with=engine)
-    q = db.select([table])
-    if filterinp == 1:
-        q = db.select([table]).where(table.columns.MntWines >= 1000)
-    if filterinp == 2:
-        q = db.select([table]).where(table.columns.NumWebPurchases > table.columns.NumStorePurchases)
-    if filterinp == 3:
-        q = db.select([table]).where(table.columns.NumStorePurchases > table.columns.NumWebPurchases)
-    if filterinp == 4:
-        q = db.select([table]).where(table.columns.Education == 'PhD')
-    if filterinp == 5:
-        q = db.select([table]).where(table.columns.Marital_Status == 'Single')
-    pandas.read_sql_query(q, connection).to_excel('output.xlsx')
+    # This uses sqlalchemy to query the database for the chosen filter type, pandas' read_sql_query()
+    # in order to instantly recieve a dataframe that is then written to a xslx file using to_excel()
+    def query_sql(self, filterqueryid=0):
+        table = db.Table('list', self.metadata, autoload=True, autoload_with=self.engine)   # Load table difinition from db
+        query = db.select([table])  # Setup default query in case filterqueryid is a weird number
+        if filterqueryid == 1:
+            query = db.select([table]).where(table.columns.MntWines >= 1000)
+            pandas.read_sql_query(query, self.connection).to_excel('BigWinesBuyers.xlsx')
+        elif filterqueryid == 2:
+            query = db.select([table]).where(table.columns.NumWebPurchases > table.columns.NumStorePurchases)
+            pandas.read_sql_query(query, self.connection).to_excel('WebBuyers.xlsx')
+        elif filterqueryid == 3:
+            query = db.select([table]).where(table.columns.NumStorePurchases > table.columns.NumWebPurchases)
+            pandas.read_sql_query(query, self.connection).to_excel('StoreBuyers.xlsx')
+        elif filterqueryid == 4:
+            query = db.select([table]).where(table.columns.Education == 'PhD')
+            pandas.read_sql_query(query, self.connection).to_excel('CustomersWithPhD.xlsx')
+        elif filterqueryid == 5:
+            query = db.select([table]).where(table.columns.Marital_Status == 'Single')
+            pandas.read_sql_query(query, self.connection).to_excel('SinglePartnersAroundYou.xlsx')
+        else:
+            pandas.read_sql_query(query, self.connection).to_excel('defaultoutput.xlsx')
